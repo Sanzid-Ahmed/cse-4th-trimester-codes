4b17ec989c996904bd6dbb294a81aa72730d9989
Sanzid Ahmed <sanzidahmed313@gmail.com>
2025-11-29 07:35:51 +0600

    added 6 no answer

--- DSAL_1 Assignment_01/No_05_Answer.c
+++ DSAL_1 Assignment_01/No_05_Answer.c
@@ -12,32 +12,20 @@ int main(){
         scanf("%d", &nums[i]);
     }
 
-    int found;
-    //for(int i = 0; i < n; i++){
         int start = 0;
-        int end = n - 1;
+        int end = n;
 
-        while(start <= end){
+        while(start < end){
             int mid = start + (end - start) / 2;
 
-            //if(nums[mid] == i){
-                //break;
-            //}
-            if(nums[mid] > mid)
-                end = mid - 1;
-            else
+
+            if(nums[mid] == mid)
                 start = mid + 1;
+            else
+                end = mid;
 
         }
 
-        //if(start > end){
-            //found = i;
-            //break;
-        //}
-    //}
-
-
-    //printf("\n%d\n", found);
     printf("\n%d\n", start);
 
     return 0;
